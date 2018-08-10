54b88e3abb7ff1b6a4e607b850fdc14e85e086dc
Valeriy Molchanov <valeriy.molchanov.77@gmail.com>
2018-08-10 23:47:38 +0300

    AngularFireModule is imported/configured in CoreModule

--- chat-spa/src/environments/environment.ts
+++ chat-spa/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBF5foLR3BzFhyIiIxKJzyzZDG-79M1T5w',
+    authDomain: 'chat-97ddc.firebaseapp.com',
+    databaseURL: 'https://chat-97ddc.firebaseio.com',
+    projectId: 'chat-97ddc',
+    storageBucket: 'chat-97ddc.appspot.com',
+    messagingSenderId: '145613974570'
+  }
 };
 
 /*
