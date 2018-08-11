01105c8f6fb9a59ea6505aafd9a0afd806ec06f2
Valeriy <valeriy.molchanov.77@gmail.com>
2018-08-11 09:11:33 +0300

    angularfire2 (#2)
    * angularfire2
    
    * AngularFireModule is imported/configured in CoreModule
    
    * title+spec
    
    * title

--- chat-spa/src/environments/environment.prod.ts
+++ chat-spa/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBF5foLR3BzFhyIiIxKJzyzZDG-79M1T5w',
+    authDomain: 'chat-97ddc.firebaseapp.com',
+    databaseURL: 'https://chat-97ddc.firebaseio.com',
+    projectId: 'chat-97ddc',
+    storageBucket: 'chat-97ddc.appspot.com',
+    messagingSenderId: '145613974570'
+  }
 };
+
