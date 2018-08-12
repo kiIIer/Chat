bbd8184307cfe6f631eee1802d390c334a8d39b6
Valeriy Molchanov <valeriy.molchanov.77@gmail.com>
2018-08-12 15:30:34 +0300

    AngularFireModule.initializeApp

--- chat-spa/src/app/app.module.ts
+++ chat-spa/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module';
@@ -12,6 +15,7 @@ import { CoreModule } from './modules/core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
     CoreModule
   ],
   providers: [],
--- chat-spa/src/app/modules/core/core.module.ts
+++ chat-spa/src/app/modules/core/core.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { environment } from '../../../environments/environment';
 
 @NgModule({
   imports: [
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
     AngularFireAuthModule
   ]
 })
